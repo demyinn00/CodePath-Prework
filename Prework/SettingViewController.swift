8b09ab7ff7def10442b97d277a69dac71ff8b3ba
David Em-Yinn <demyinn@ucsd.edu>
2021-12-15 14:00:13 -0800

    added another view and need to design settings

---
+++ Prework/SettingViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingViewController.swift
+//  Prework
+//
+//  Created by David Em-Yinn on 12/15/21.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        // On saving and loading portion of the tutorial...
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
